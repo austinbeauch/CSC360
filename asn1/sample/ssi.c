e72be30efaed45eae5ec9aae3d2e77771c776d46
Austin Beauchamp <austinbeauch@gmail.com>
2018-01-18 17:47:21 -0800

    Tokenize input, execute with execvp

--- asn1/sample/ssi.c
+++ asn1/sample/ssi.c
@@ -13,33 +13,37 @@ int main(){
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		perror("getcwd() error");
 		
+	// prompt generation
 	strcpy (str, "SSI: ");
 	strcat (str, cwd);
 	strcat (str, " > ");
-
-	//const char* prompt = "SSI: " + cwd + " > ";
 	
 	int bailout = 0;
 	while (!bailout) {
 
 		char* reply = readline(str);
+		
+		// string tokenization
+		char* token = strtok(reply, " ");
+		char* tokens[256];
+		int index = 0;
+		while (token) {
+			tokens[index] = token;
+			index++;
+			token = strtok(NULL, " ");
+		}
+		tokens[index] = NULL;
 
 		if (!strcmp(reply, "exit")) {
 			bailout = 1;
 		} else {
-			printf("You said: %s\n", reply);
 			pid_t p = fork();
-			if(p == 0) {
-				char* arg[] = {"ls", "-a", "..", NULL};
-				execvp(arg[0], arg);
-				printf("Inside child process: %d\n", p);
-			}
-			else if (p > 0) {
+			
+			if(p == 0) { // in child process				
+				execvp(tokens[0], tokens);
+			} else if (p > 0) { // in parent process
 				wait(NULL);
-				printf("In parent process; child PID: %d\n", p);
-				exit(0);
-			}
-			else {
+			} else {
 				perror("ERROR IN FORK");
 				exit(-1);
 			}
