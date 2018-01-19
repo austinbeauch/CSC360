957903ed8d690cee1810b93b457f607496bf3de1
Austin Beauchamp <austinbeauch@gmail.com>
2018-01-19 12:13:02 -0800

    Add change directory functionality

--- asn1/sample/ssi.c
+++ asn1/sample/ssi.c
@@ -7,20 +7,19 @@
 #include <readline/history.h>
 
 int main(){
-	char str[80];
-	char cwd[256];
-	
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-		perror("getcwd() error");
-		
-	// prompt generation
-	strcpy (str, "SSI: ");
-	strcat (str, cwd);
-	strcat (str, " > ");
 	
 	int bailout = 0;
 	while (!bailout) {
-
+		char str[80];
+		char cwd[256];
+		
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			perror("getcwd() error");
+			
+		// prompt generation
+		strcpy (str, "SSI: ");
+		strcat (str, cwd);
+		strcat (str, " > ");
 		char* reply = readline(str);
 		
 		// string tokenization
@@ -33,14 +32,25 @@ int main(){
 			token = strtok(NULL, " ");
 		}
 		tokens[index] = NULL;
-
 		if (!strcmp(reply, "exit")) {
 			bailout = 1;
+			free(reply);
+			return 0;
 		} else {
 			pid_t p = fork();
 			
-			if(p == 0) { // in child process				
-				execvp(tokens[0], tokens);
+			if(p == 0) { // in child process	
+				if(!strcmp(tokens[0], "cd")){
+					if(tokens[1] == NULL || !strcmp(tokens[1], "~")){
+						chdir(getenv("HOME"));
+					}
+					else{
+						chdir(tokens[1]);	
+					}
+				}
+				else {
+					execvp(tokens[0], tokens);
+				}
 			} else if (p > 0) { // in parent process
 				wait(NULL);
 			} else {
@@ -51,5 +61,5 @@ int main(){
 		}
 		free(reply);
 	}
-	printf("Bye\n"); 
+	return 0;
 }
