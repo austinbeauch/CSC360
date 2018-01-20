215a82efefd67a195cd8ccd0d367db8cef3d3e3d
Austin <austinbeauch@gmail.com>
2018-01-19 23:13:47 -0800

    Start background process handling

--- asn1/sample/ssi.c
+++ asn1/sample/ssi.c
@@ -7,21 +7,20 @@
 #include <readline/history.h>
 
 int main(){
-	
 	int bailout = 0;
 	while (!bailout) {
 		char str[80];
 		char cwd[256];
-		
+
 		if (getcwd(cwd, sizeof(cwd)) == NULL)
 			perror("getcwd() error");
-			
+
 		// prompt generation
 		strcpy (str, "SSI: ");
 		strcat (str, cwd);
 		strcat (str, " > ");
 		char* reply = readline(str);
-		
+
 		// string tokenization
 		char* token = strtok(reply, " ");
 		char* tokens[256];
@@ -32,23 +31,38 @@ int main(){
 			token = strtok(NULL, " ");
 		}
 		tokens[index] = NULL;
+
 		if (!strcmp(reply, "exit")) {
 			exit(0);
-		} 
-		if(!strcmp(tokens[0], "cd")){
+		}
+		else if(!strcmp(tokens[0], "cd")){
 			int x;
 			if(tokens[1] == NULL || !strcmp(tokens[1], "~")){
 				int x = chdir(getenv("HOME"));
 			} else {
-				int x = chdir(tokens[1]);	
+				int x = chdir(tokens[1]);
 			}
 			if(x == -1){
 				printf("chdir error\n");
 			}
 		}
+		else if(!strcmp(tokens[0], "bg")){
+			pid_t p = fork();
+			if(p == 0) { // in child process
+				execvp(tokens[1], tokens + 1);
+			} else if (p > 0) { // in parent process
+				waitpid(p, NULL, WNOHANG);
+			} else {
+				perror("ERROR IN FORK");
+				exit(-1);
+			}
+		}
+		else if(!strcmp(tokens[0], "bglist")){
+
+		}
 		else {
 			pid_t p = fork();
-			if(p == 0) { // in child process	
+			if(p == 0) { // in child process
 				execvp(tokens[0], tokens);
 			} else if (p > 0) { // in parent process
 				wait(NULL);
@@ -56,7 +70,7 @@ int main(){
 				perror("ERROR IN FORK");
 				exit(-1);
 			}
-			
+
 		}
 		free(reply);
 	}
